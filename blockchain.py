86011900e692dbb1582c1ccbe1259daab3b289f2
702260 <58435650+702260@users.noreply.github.com>
2024-09-11 17:43:37 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -33,3 +33,25 @@ def register_node(self, address):
      self.nodes.add(parsed_url.path)
   else
      raise ValueError('Invalid URL')
+
+def valid_chain(self, chain):
+   """
+   Determine ifa given blockchain is valid
+
+   :param chain: A blockchain
+   :return: True if valid, False if not
+   """
+
+   last_block = chain[0]
+
+   current_index = 1
+
+   while current_index < len(chain):
+      block = chain[current_index]
+      print(f'{last_block'})
+      print(f'{block}')
+      print("\n--------\n")
+      # check that the hash of the block is correct
+
+      last_block_hash = self.hash(last_block)
+      if block['previous_hash'] != last_block_hash;
