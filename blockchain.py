e2cf6b82a05d3fa4c4b7ae8a30f4182665beed2c
702260 <58435650+702260@users.noreply.github.com>
2024-09-17 16:45:03 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -69,4 +69,30 @@ def register_node(self, address):
 
      def resolve_conflicts(self):
      """
-  
+     This is our consensus algorithm, it resolves conflicts
+     by replacing our chain with the the longest one in the network.
+
+     :return: True if our chain was replaced, False if not
+     """
+
+     neighbours = self.nodes
+     new_chain = None
+
+    # We're only looking for chains longer than ours
+
+    max_length = len(self.chain)
+
+    # Grab and verify the chains from all the nodes in our network
+    for node in neighbours:
+        response = requests.get(f'http://{node}/chain')
+
+        if response.status_code == 200:
+            length = response.json()['length]
+            chain = response.json()['chain']
+
+            # check if the length is longer and the chain is valid
+
+            if length > max_length and self.valid_chain(chain):
+            
+     
+    
