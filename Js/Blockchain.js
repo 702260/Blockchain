3efc860a5fb8abf6145a0adf483ab498b738453f
702260 <58435650+702260@users.noreply.github.com>
2025-02-05 10:41:23 +0530

    Update Blockchain.js

--- Js/Blockchain.js
+++ Js/Blockchain.js
@@ -7,6 +7,25 @@ class Blockchain {
     this.newBlock();
     this.peers = new Set();
   }
+
+  /**
+  * adds a node to our peer table
+  */
+
+  addPeer(host) {
+    this.peers.add(host);
+  }
+
+  /**
+  * adds a node to our peer table
+  */
+  getPeers() {
+       return Array.from(this.peers);
+  }
+  
+  
+  
+  
   
 
     
