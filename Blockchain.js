7fe3e9495285e4af34d50babcc5062f665ecb862
702260 <58435650+702260@users.noreply.github.com>
2025-02-13 12:07:20 +0530

    Update Blockchain.js

--- Blockchain.js
+++ Blockchain.js
@@ -6,6 +6,26 @@ class Blockchain
      this.chain = [];
      this.pendingtransaction = [];
      this.newBlock();
+     this.peers = new Set();
+   }
+
+    /**
+    * Adds a node to our peer
+    */
+    addPeer(host) {
+      this.peers.add(host);
+    }
+
+    /**
+   * Adds a node to our peer table
+   */
+    getPeers() {
+      return Array.from(this.peers);
+
+  
+    
+    
+     
      
      
      
