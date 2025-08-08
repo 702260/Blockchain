3827c757533392e866b31353b2380cbcddaff15c
702260 <58435650+702260@users.noreply.github.com>
2025-08-08 18:07:33 +0530

    Update WebServer.cs

--- csharp/blockchain/WebServer.cs
+++ csharp/blockchain/WebServer.cs
@@ -11,6 +11,12 @@ namespace BlockChainDemo
     public WebServer(BlockChain chain)
     {
       var settings = ConfigurationManager.Appsettings;
+      string host = settings["host"]?.Length > 1 settings["host"] : "localhost";
+      string port = settings["port"]?.Length > 1 settings["port"] : "12345";
+
+      var server = new TinyWebServer.WebServer(request =>
+                   {
+       
       
   
 
