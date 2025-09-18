58e7cf683cbc71d6dbf43983fca7e8b37bafa154
702260 <58435650+702260@users.noreply.github.com>
2025-09-18 16:09:16 +0400

    Update Block.cs

--- csharp/blockchain/Block.cs
+++ csharp/blockchain/Block.cs
@@ -3,4 +3,8 @@ using System.Collections.generic;
 
 namespace BlockChainDemo
 {
-  public class Blo
+  public class Block
+  {
+    public int Index {get; set;}
+    public Datetime Timestamp {get; set;}
+    public List<transaction> Transactions {get;  set;}
