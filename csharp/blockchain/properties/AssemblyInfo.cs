6b0ba5f7365963a175fa837068dc7ee920b4448c
702260 <58435650+702260@users.noreply.github.com>
2025-10-01 13:44:48 +0400

    Update AssemblyInfo.cs

--- csharp/blockchain/properties/AssemblyInfo.cs
+++ csharp/blockchain/properties/AssemblyInfo.cs
@@ -2,4 +2,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.Interopservices;
 
-//General Information 
+//General Information  about an assembly is controlled through the followilng
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
