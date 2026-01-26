df63f931e922de7f9585e32394f2c728d7508176
702260 <58435650+702260@users.noreply.github.com>
2026-01-26 09:15:38 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -17,21 +17,21 @@ class Blockchain:
         # Create the genesis block
         self.new_block(previous_hash = '1', proof = 100)
 
-   def register_node(self, address):
-       """
-       Add a new node to the list of nodes
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
 
-       :param address: Address of node. Eg. 'http://192.168.0.5:5000'
-       """
+        :param address: Address of node. Eg. 'http://192.168.0.5:5000'
+        """
 
-       parsed_url = urlparse(address)
-       if parsed_url.netloc:
-           self.nodes.add(parsed_url.netloc)
-       elif parsed_url.path:
-           # Accepts an URL without scheme like '192.168.0.5:5000'.
-           self.nodes.add(parsed_url.path)
-       else:
-           raise ValueError('Invalid URL')
+        parsed_url = urlparse(address)
+        if parsed_url.netloc:
+            self.nodes.add(parsed_url.netloc)
+        elif parsed_url.path:
+            # Accepts an URL without scheme like '192.168.0.5:5000'.
+            self.nodes.add(parsed_url.path)
+        else:
+            raise ValueError('Invalid URL')
 
 
     def valid_chain(self, chain):
